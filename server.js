0a28beb2aefe7e6d6e0ce5245f2f8d9a467a4169
qwer-julia <79732591+qwer-julia@users.noreply.github.com>
2024-06-18 13:11:01 -0300

    removendo as rotas do http

--- server.js
+++ server.js
@@ -1,11 +1,6 @@
 import app from "./src/app.js";
 
 const PORT = 3000;
-const rotas = {
-    "/": "Curso de node.js",
-    "/livros": "Entrei na rota livros",
-    "/autores": "Entrei na rota autores"
-}
 
 app.listen(PORT, ()=> {
     console.log("servidor escutando!");
