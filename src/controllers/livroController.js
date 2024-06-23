16639ff4a953d410550ec72a78b8ecebb46f4196
qwer-julia <79732591+qwer-julia@users.noreply.github.com>
2024-06-23 15:47:34 -0300

    adicionando busca por query de editora

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -70,6 +70,16 @@ class LivroController {
             })
         }
     }
+
+    static async listarLivrosPorEditora (req, res) {
+        try {
+            const editora = req.query.editora;
+            const livrosPorEditora = await livro.find({ editora: editora });
+            res.status(200).json(livrosPorEditora);
+        } catch (erro) {
+            res.status(500).json({ message: `${erro.message} - falha na busca` });
+        }
+    }
 };
 
 export default LivroController;
--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -4,6 +4,7 @@ import LivroController from "../controllers/livroController.js";
 const routes = express.Router();
 
 routes.get("/livros", LivroController.listarLivros);
+routes.get("/livros/busca", LivroController.listarLivrosPorEditora);
 routes.post("/livros", LivroController.cadastrarLivro);
 routes.get("/livros/:id", LivroController.listarLivroPorId);
 routes.put("/livros/:id", LivroController.atualizarLivro);
