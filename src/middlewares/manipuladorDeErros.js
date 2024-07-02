32da66fe31e687a335b1a662f3ee69c671f14dbf
qwer-julia <79732591+qwer-julia@users.noreply.github.com>
2024-07-02 11:59:52 -0300

    erros personalizados e erros de validacao add no middleware

--- src/middlewares/manipuladorDeErros.js
+++ src/middlewares/manipuladorDeErros.js
@@ -5,6 +5,11 @@ import mongoose from "mongoose";
 function manipuladorDeErros(erro, req, res, next) {
   if(erro instanceof mongoose.Error.CastError) {
     res.status(400).send({message: "Um ou mais dados fornecidos estao incorretos", erro});
+  } else if (erro instanceof mongoose.Error.ValidationError) {
+    const mensagensErro = Object.values(erro.errors)
+      .map(erro => erro.message)
+      .join("; ");
+    res.status(400).send({message: "Houve um erro de validação de dados", error: mensagensErro});
   } else {
     res.status(500).send({message: "Erro interno de servidor", erro});
   }
--- src/models/Livro.js
+++ src/models/Livro.js
@@ -2,14 +2,14 @@ import mongoose from "mongoose";
 
 const livroSchema = new mongoose.Schema({
   id: { type: mongoose.Schema.Types.ObjectId },
-  titulo: { type: mongoose.Schema.Types.String, required: true },
+  titulo: { type: mongoose.Schema.Types.String, required: [true, "O título do livro é obrigatório"] },
   editora: { type: mongoose.Schema.Types.String },
   preco: { type: mongoose.Schema.Types.Number },
   paginas: { type: mongoose.Schema.Types.Number },
   autor: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "autores",
-    required: [true, "O autor é obrigatório"]
+    required: [true, "O(a) autor(a) é obrigatório"]
   },
 }, {versionKey: false});
 
