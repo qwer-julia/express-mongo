a67a4e2029efda1668eb71c142ae99d813ecb57d
qwer-julia <79732591+qwer-julia@users.noreply.github.com>
2024-08-07 12:53:09 -0300

    Validações mongoose

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -5,7 +5,11 @@ const livroSchema = new mongoose.Schema({
   titulo: { type: mongoose.Schema.Types.String, required: [true, "O título do livro é obrigatório"] },
   editora: { type: mongoose.Schema.Types.String },
   preco: { type: mongoose.Schema.Types.Number },
-  paginas: { type: mongoose.Schema.Types.Number },
+  paginas: {
+    type: mongoose.Schema.Types.Number,
+    min: [10, "O numero de paginas deve estar entre 10 e 5000. Valor fornecido: {VALUE}."],
+    max: [5000, "O numero de paginas deve estar entre 10 e 5000. Valor fornecido: {VALUE}."]
+  },
   autor: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "autores",
